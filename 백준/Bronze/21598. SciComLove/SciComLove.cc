ac0e913e8b37dc85dfd4cb1ea5084880730a3c9c
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:39:37 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long
+
+int main(void)
+{
+    u;
+    int n;
+    cin >> n;
+    if (n == 1) {
+        cout << "SciComLove";
+    }
+    else {
+        cout << "SciComLove\nSciComLove";
+    }
+}
