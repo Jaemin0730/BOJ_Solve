dfcd24a343695dc7b4762f1ecb7dfdf298437a9a
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:23:46 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#pragma warning(disable : 4996)
+
+/*
+	
+
+*/
+
+int main() {
+	int n, k;
+	int a, b;
+	scanf("%d %d", &n, &k);
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &a, &b);
+	}
+	printf("비와이");
+	return 0;
+}
