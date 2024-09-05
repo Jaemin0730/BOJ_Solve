aa6d16603c8c313e9dc2cbb4aaa937b7231e19e5
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:26:11 +0900

    [Bronze V] Title: Rectangles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+
+
+int main(void)
+{
+    u;
+    int a, b;
+    cin >> a >> b;
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < b; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
