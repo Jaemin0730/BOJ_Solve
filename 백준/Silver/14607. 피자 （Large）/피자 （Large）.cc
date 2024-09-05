cee42e7dfc5844bf5039ed806ebe98c93a14220c
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:23:35 +0900

    [Silver III] Title: 피자 (Large), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14607. 피자 （Large）/피자 （Large）.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u cin.tie(0)->sync_with_stdio(0)
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+int main(void)
+{
+    u;
+    ll n;
+    cin >> n;
+    cout << n * (n - 1) / 2;
+}
