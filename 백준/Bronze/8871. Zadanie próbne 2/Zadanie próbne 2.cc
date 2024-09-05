c8d044adec6586c92a1758e5a695cc9b7058b29c
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 13:57:26 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <map>
+#include <list>
+#include <cmath>
+#include <set>
+using namespace std;
+#define ll long long
+
+void g(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main(int argc, char **argv)
+{
+    g();
+    int n;
+    cin >> n;
+    cout << 2 * (n + 1) << " " << 3 * (n + 1);
+}
