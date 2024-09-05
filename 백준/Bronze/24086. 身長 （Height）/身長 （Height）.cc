3bf6f8f421ef889e3e8bdc65a5a5a7a888f30794
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:42:09 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <map>
+#include <list>
+#include <cmath>
+#include <set>
+using namespace std;
+#define ll long long
+
+void g(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main(int argc, char **argv)
+{
+    g();
+    int a, b;
+    cin >> a >> b;
+    cout << b - a;
+}
