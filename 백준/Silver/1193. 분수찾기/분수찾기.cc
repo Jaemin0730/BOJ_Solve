6d54898b1fae83c9443a88ae4d91f6b8a30405b2
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 10:15:25 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#pragma warning(disable : 4996)
+
+/*
+	
+
+*/
+
+int main() {
+	int a;
+	int b = 1;
+	int i;
+
+	scanf("%d", &a);
+
+	for (i = 1; a > b;) {
+		b += ++i;
+	}
+
+	if (i % 2 == 0) {
+		printf("%d/%d", i - (b - a), 1 + (b - a));
+	}
+	else {
+		printf("%d/%d", 1 + (b - a), i - (b - a));
+	}
+
+	return 0;
+}
