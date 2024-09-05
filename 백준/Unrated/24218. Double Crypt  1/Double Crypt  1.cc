1ceffa9bba9e2cd62a1e81afb31e1aadcd6eb297
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:42:28 +0900

    [Unrated] Title: Double Crypt  1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Unrated/24218. Double Crypt  1/Double Crypt  1.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <map>
+#include <list>
+#include <cmath>
+#include <set>
+#include <iomanip>
+using namespace std;
+#define ll long long
+
+void g(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main(int argc, char **argv)
+{
+    g();
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        string a, b;
+        cin >> a >> b;
+        cout << "A0000000000000000000000000000000"<<"\n";
+        cout << "70000000000000000000000000000000"<<"\n";
+    }
+}
