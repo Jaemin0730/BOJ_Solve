bb29e16a770148571840e86dcca81a3e65490e3e
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:18:29 +0900

    [Bronze IV] Title: 파일 옮기기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11943. 파일 옮기기/파일 옮기기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <map>
+#include <cmath>
+using namespace std;
+#define ll long long
+
+void g(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main(void)
+{
+    g();
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    int sol = min(a + d, b + c);
+    cout << sol;
+}
