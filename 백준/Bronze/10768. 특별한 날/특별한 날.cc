0d165e28c0b4162e37c89a95175ab590cd5a2917
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:04:30 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.cc
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	int month, day;
+	scanf("%d %d", &month, &day);
+
+	if (month == 2 && day == 18) {
+		printf("Special");
+	}
+	
+	if (month < 2) {
+		printf("Before");
+	}
+	else if (month > 2) {
+		printf("After");
+	}
+	else {
+		if (day < 18)
+			printf("Before");
+		else if (day > 18) 
+			printf("After");
+	}
+}
