d18075ca25f9edc35246cbeb54a4382413ca6750
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:43:59 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long
+#define pr pair<int, int>
+
+int main(void)
+{
+    u;
+    for (int i = 0; i < 2; i++) {
+        int a, b, c, d, e;
+        cin >> a >> b >> c >> d >> e;
+        cout << a * 6 + b * 3 + c * 2 + d + e * 2 << " ";
+    }
+}
