b4872bafb7d1319b4b6ae04b57cc7938e1a6f81e
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 13:57:21 +0900

    [Bronze IV] Title: Schronisko, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8760. Schronisko/Schronisko.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+
+
+int main(void)
+{
+    u;
+    int n, m, k;
+    cin>>k;
+    for(int i = 0; i < k; i++){
+        cin >> n >> m;
+        cout << n * m / 2 << "\n";
+    }
+}
