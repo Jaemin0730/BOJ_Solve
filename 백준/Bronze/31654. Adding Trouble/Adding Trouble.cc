5ad6317b603f09744362ca323567ec8b48cff3ea
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:59:40 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u                                                                      \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(NULL);
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main(void) {
+    u
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a + b == c) cout << "correct!";
+    else cout << "wrong!";
+}
