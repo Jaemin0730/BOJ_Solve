85d4f9edaad3fe78da6ee03b1a009043c0c79667
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:55:38 +0900

    [Bronze IV] Title: Rust Study, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30033. Rust Study/Rust Study.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = list(input().split())
+b = list(input().split())
+
+sum = 0
+
+for i in range(0, n) : 
+    if int(a[i]) <= int(b[i]) :
+        sum += 1
+        
+print(sum)
