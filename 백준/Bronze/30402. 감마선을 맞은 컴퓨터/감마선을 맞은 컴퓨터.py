4a73154bb7ce2c1d8da591791571f30b2135c56e
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:56:18 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,13 @@
+a = []
+for i in range(0, 15) :
+    a = list(map(str, input().split()))
+    
+    if 'w' in a :
+        print('chunbae')
+        break;
+    elif 'b' in a :
+        print('nabi')
+        break;
+    elif 'g' in a :
+        print('yeongcheol')
+        break;
