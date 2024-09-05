50babd2dfa04c90d026e424400a3357fe2075833
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 10:29:06 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	cout << n * m - 1;
+	return 0;
+}
