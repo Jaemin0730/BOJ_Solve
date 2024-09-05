0a0422d6bd1700bd628fbb197826febbed83dac0
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:15:23 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 36 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <map>
+#include <list>
+#include <cmath>
+#include <set>
+using namespace std;
+#define ll long long
+
+void g(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main(void)
+{
+    g();
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int p = 0;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+        for (int i = 0; i < n - 2; i++) {
+            p = max(p, abs(v[i] - v[i + 2]));
+        }
+        cout << p << "\n";
+    }
+}
