61d9c7400fd07828812959d67ad02651b7fa774b
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:45:39 +0900

    [Bronze IV] Title: 샤틀버스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+
+
+int main(void)
+{
+    u;
+    int x, y;
+    cin >> x >> y;
+    if (x > y) cout << x + y;
+    else cout << y - x;
+}
