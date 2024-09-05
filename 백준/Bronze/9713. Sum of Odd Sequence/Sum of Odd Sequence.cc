54a30e3f262139c9a3cf0d1d2a400c446b0636c7
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:01:23 +0900

    [Bronze III] Title: Sum of Odd Sequence, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9713. Sum of Odd Sequence/Sum of Odd Sequence.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <map>
+#include <list>
+#include <cmath>
+#include <set>
+using namespace std;
+#define ll long long
+
+void z()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main(void)
+{
+    z();
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int sum = 0;
+        for (int i = 1; i <= n; i += 2)
+            sum += i;
+        cout << sum << "\n";
+    }
+}
