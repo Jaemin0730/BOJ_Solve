5002abfea9464521f26eb0b667a0159a76548111
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:21:10 +0900

    [Bronze IV] Title: Andando no tempo, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13580. Andando no tempo/Andando no tempo.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+
+
+int main(void)
+{
+    u;
+    int a[3];
+    for(int i = 0; i < 3; i++) {
+        cin >> a[i];
+    }
+    sort(a, a + 3);
+    if (a[0] == a[1] || a[1] == a[2] || a[0] + a[1] == a[2]) {
+        cout << "S";
+    }
+    else {
+        cout << "N";
+    }
+}
