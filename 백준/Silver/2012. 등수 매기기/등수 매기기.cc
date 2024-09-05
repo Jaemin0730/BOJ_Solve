09271de43ed610187ad51d2a13a8aa19a6f28429
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 10:28:11 +0900

    [Silver III] Title: 등수 매기기, Time: 84 ms, Memory: 5220 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <map>
+#include <list>
+#include <cmath>
+#include <set>
+using namespace std;
+#define ll long long
+
+void g(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+
+int main(void)
+{
+    g();
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        v.push_back(num);
+    }
+    sort(v.begin(), v.end());
+    ll sol = 0;
+    for (int i = 1; i <= n; i++) {
+        sol += abs(i - v[i - 1]);
+    }
+    cout << sol;
+}
