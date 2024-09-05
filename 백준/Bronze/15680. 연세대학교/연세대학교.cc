cb1e3af54e6100835748466800c66cf3e902d9b8
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:28:27 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#pragma warning(disable : 4996)
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	if (n == 1) {
+		printf("Leading the Way to the Future\n");
+	}
+	else {
+		printf("YONSEI\n");
+	}
+
+	return 0;
+}
