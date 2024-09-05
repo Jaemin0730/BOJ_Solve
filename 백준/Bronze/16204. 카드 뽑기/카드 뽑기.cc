33a0d4befe0b201412fd2badcbf467172aa0fb79
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:31:24 +0900

    [Bronze IV] Title: 카드 뽑기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16204. 카드 뽑기/카드 뽑기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <map>
+#include <list>
+#include <cmath>
+#include <set>
+using namespace std;
+#define ll long long
+
+void g(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main(void)
+{
+    g();
+    int n, m, k;
+    cin >> n >> m >> k;
+    cout << min(m, k) + min(n - m, n - k);
+}
