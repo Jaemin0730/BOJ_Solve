e1f9d9d45caa2840951f1b2608a94d2d111e1589
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 13:54:24 +0900

    [Bronze IV] Title: ISBN, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u cin.tie(0)->sync_with_stdio(0)
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+int main(void)
+{
+    u;
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << "The 1-3-sum is " << 91 + a * 1 + b * 3 + c * 1;
+}
