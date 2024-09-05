108e82f0f1459c881e250563b9a03f71e7ed38e7
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 13:59:22 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL);
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+
+
+int main(void)
+{
+    u
+    int t;
+    cin >> t;
+    while (t--){
+        int n, m;
+        cin >> n >> m;  
+        for (int i = 0; i < m; i++){
+            int a, b; 
+            cin >> a >> b; 
+        }
+        cout << n - 1 << "\n";
+    }
+}
