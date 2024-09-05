6d5534ac23c92372d41b6e634bb56fb4de00e6a9
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:31:54 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#pragma warning(disable : 4996)
+
+/*
+	
+
+*/
+
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	float result = 2 * a + 2 * b * 3.141592;
+	printf("%f", result);
+	return 0;
+}
