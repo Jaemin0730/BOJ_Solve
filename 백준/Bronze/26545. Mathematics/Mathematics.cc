a0e73d84e4cc47291fd137c2435823a734109270
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:47:55 +0900

    [Bronze V] Title: Mathematics, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL);
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+
+
+int main(void)
+{
+    u
+    int n;
+    cin >> n;
+    int a = 0;
+    while (n--) {
+        int k;
+        cin >> k;
+        a += k;
+    }
+    cout << a;
+}
