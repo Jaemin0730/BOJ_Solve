8ec1f741e0067740c916975f7b6a7e973421bf6c
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:35:06 +0900

    [Bronze IV] Title: FYI, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long
+
+int main(void)
+{
+    u;
+    string s;
+    cin >> s;
+    cout << (s.substr(0, 3) == "555" ? "YES" : "NO") << "\n";
+}
