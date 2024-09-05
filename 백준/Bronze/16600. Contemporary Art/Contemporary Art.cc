ede78bafacc7664b2066af1d602e03e015bb7f8e
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:32:19 +0900

    [Bronze IV] Title: Contemporary Art, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/16600. Contemporary Art/Contemporary Art.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long
+
+int main(void)
+{
+    u;
+    double n;
+    cin >> n;
+    double a = 4 * sqrt(n);
+    printf("%.7lf\n", a);
+}
