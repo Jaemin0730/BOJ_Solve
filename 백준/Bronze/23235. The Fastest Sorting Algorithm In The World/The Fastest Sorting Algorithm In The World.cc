7b3391dd85d12327f69bbdb96d49ca1d73fce1cc
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:40:52 +0900

    [Bronze V] Title: The Fastest Sorting Algorithm In The World, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/23235. The Fastest Sorting Algorithm In The World/The Fastest Sorting Algorithm In The World.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+
+
+int main(void)
+{
+    u;
+    string s;
+    for (int i = 1;; i++)
+    {
+        getline(cin, s);
+        if (s == "0")
+            break;
+        printf("Case %d: Sorting... done!\n", i);
+    }
+}
