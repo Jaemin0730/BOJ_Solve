5442cdce77c475463ee5e3d8375bfc98e5061039
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:04:46 +0900

    [Bronze IV] Title: 10부제, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.cc
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+	int day;
+    int car[5];
+	scanf("%d", &day);
+	for (int i = 0; i < 5; i++) {
+		scanf("%d", &car[i]);
+	}
+
+	int count = 0;
+	for (int i = 0; i < 5; i++) {
+		if (day == car[i]) {
+            count++;
+        }
+	}
+    
+	printf("%d", count);
+	return 0;
+}
