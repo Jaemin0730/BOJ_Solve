b6f3441f3419a3038e85dd6f6a852e3b24557cd0
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 10:37:11 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#pragma warning(disable : 4996)
+
+/*
+	
+
+*/
+
+int main() {
+	int num;
+
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++) {
+		printf("%d\n", i + 1);
+	}
+	return 0;
+}
