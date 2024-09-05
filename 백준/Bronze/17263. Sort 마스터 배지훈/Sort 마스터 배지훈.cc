14b50ff4e715c1cf640cf4fc7ec5c5d49a71fb33
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:33:35 +0900

    [Bronze III] Title: Sort 마스터 배지훈, Time: 76 ms, Memory: 5220 KB -BaekjoonHub

---
+++ 백준/Bronze/17263. Sort 마스터 배지훈/Sort 마스터 배지훈.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    vector<int> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        v.push_back(num);
+    }
+    sort(v.begin(), v.end());
+    cout << v[n - 1];
+}
