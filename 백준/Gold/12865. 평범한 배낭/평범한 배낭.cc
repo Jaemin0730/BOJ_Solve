b08e880e1c7183c9b55e1c27e95680fd4125b8c0
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:19:29 +0900

    [Gold V] Title: 평범한 배낭, Time: 32 ms, Memory: 41352 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+int main(void)
+{
+    u;
+    int n, k;
+    cin >> n >> k;
+    int w[n + 1], v[n + 1], dp[n + 1][100001] = {};
+    for (int i = 1; i <= n; i++) {
+        cin >> w[i] >> v[i];
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= k; j++) {
+            if (w[i] > j) {
+                dp[i][j] = dp[i - 1][j];
+            }
+            else {
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+            }
+        }
+    }
+    cout << dp[n][k];
+}
