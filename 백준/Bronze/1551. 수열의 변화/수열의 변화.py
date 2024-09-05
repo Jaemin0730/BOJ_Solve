df5da309d45e848578d71cf25d518e50f72fe537
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 10:20:28 +0900

    [Bronze I] Title: 수열의 변화, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/1551. 수열의 변화/수열의 변화.py
@@ -0,0 +1,8 @@
+n, k = map(int, input().split())
+a = list(map(int, input().split(',')))
+for _ in range(k):
+    b = []
+    for i in range(1, len(a)):
+        b.append(a[i] - a[i-1])
+    a = b
+print(*a, sep = ',')
