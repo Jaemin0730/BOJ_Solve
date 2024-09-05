3e8450dc082bd7f10baece4a8029cca4075b9fa9
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:12:43 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#pragma warning(disable : 4996)
+
+/*
+
+
+*/
+
+int main() {
+	int T;
+	int num1, num2;
+
+	scanf("%d", &T);
+	for (int i = 1; i <= T; i++) {
+		scanf("%d %d", &num1, &num2);
+		printf("Case #%d: %d + %d = %d\n", i, num1, num2, num1 + num2);
+	}
+	return 0;
+}
