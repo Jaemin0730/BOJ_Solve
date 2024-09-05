5fd9ba6402446fe703af8faf9648b024a56d0f69
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 13:51:48 +0900

    [Bronze III] Title: 터널의 입구와 출구, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5612. 터널의 입구와 출구/터널의 입구와 출구.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL);
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+
+
+int main(void)
+{
+    u
+    int n, m;
+	cin >> n >> m;
+	int a, b;
+	int k = m;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		m += a - b;
+		if (m < 0) {
+			k = 0;
+			break;
+		}
+		if (m >= k) {
+			k = m;
+        }
+	}
+	cout << k;
+}
