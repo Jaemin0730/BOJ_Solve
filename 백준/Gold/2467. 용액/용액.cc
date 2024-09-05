e2b88fea85a71e3c5199cc4cf6ad394818e52a00
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 10:32:33 +0900

    [Gold V] Title: 용액, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u cin.tie(0)->sync_with_stdio(0)
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef pair<int,int> pii;
+
+vector<int> v;
+
+int main(void)
+{
+    u;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        v.pb(num);
+    }
+    sort(v.begin(), v.end());
+    int l = 0, r = n - 1;         // 투포인터에서 좌 = l, 우 = r
+    int sum = 0, ll = 0, rr = 0;  // ll은 v에서 l의 값, rr은 v에서 r의 값!
+    int sol = 2000000001;         // sol은 0에 가장 가까운 값의 합!
+    // 투포인터 개념 사용!!
+    while (l < r) {
+        sum = v[l] + v[r];
+        if (sol > abs(sum)) {
+            sol = abs(sum);
+            ll = v[l];
+            rr = v[r];
+            if (sum == 0) {
+                break;
+            }
+        }
+        if (sum < 0) {
+            l++;
+        }
+        else {
+            r--;
+        }
+    }
+    cout << ll << " " << rr;
+}
