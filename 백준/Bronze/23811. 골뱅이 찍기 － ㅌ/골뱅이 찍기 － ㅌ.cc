eb0de7dced3048a0356fb2e897ac3348c9a15f27
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:41:23 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <map>
+#include <list>
+#include <cmath>
+#include <set>
+using namespace std;
+#define ll long long
+
+void z()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main(void)
+{
+    z();
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        for (int j = 0; j < t; j++) {
+            cout << "@@@@@";
+        }
+        cout << "\n";
+    }
+    //cout << "\n";
+    for (int i = 0; i < t; i++) {
+        for (int j = 0; j < t; j++) {
+            cout << "@";
+        }
+        cout << "\n";
+    }
+    //cout << "\n";
+    for (int i = 0; i < t; i++) {
+        for (int j = 0; j < t; j++) {
+            cout << "@@@@@";
+        }
+        cout << "\n";
+    }
+    //cout << "\n";
+    for (int i = 0; i < t; i++) {
+        for (int j = 0; j < t; j++) {
+            cout << "@";
+        }
+        cout << "\n";
+    }
+    //cout << "\n";
+    for (int i = 0; i < t; i++) {
+        for (int j = 0; j < t; j++) {
+            cout << "@@@@@";
+        }
+        cout << "\n";
+    }
+}
