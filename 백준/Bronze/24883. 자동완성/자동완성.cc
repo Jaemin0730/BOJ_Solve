fc238d2b8c031ec214f6de13d5383dd1b7be4e7a
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:44:14 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long
+
+int main(void)
+{
+    u;
+    char c;
+    cin >> c;
+    if (c == 'N' || c == 'n') {
+        cout << "Naver D2";
+    }
+    else {
+        cout << "Naver Whale";
+    }
+}
