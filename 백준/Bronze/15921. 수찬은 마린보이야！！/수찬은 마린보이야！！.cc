0ca3b99718ace5f265e6fa338f4a1339a0093f60
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:30:08 +0900

    [Bronze III] Title: 수찬은 마린보이야!!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15921. 수찬은 마린보이야！！/수찬은 마린보이야！！.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    if (n != 0) {
+        cout << "1.00";
+    }
+    else {
+        cout << "divide by zero";
+    }
+}
