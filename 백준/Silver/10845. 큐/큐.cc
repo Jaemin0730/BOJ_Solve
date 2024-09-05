7685e3ab5c345b3ce1c8e888f701f719e3c8a9d6
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:07:02 +0900

    [Silver IV] Title: 큐, Time: 352 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+	queue <int> list;
+
+	for (int i = 0; i < n; i++) {
+		string word;
+		cin >> word;
+
+		if (word == "push") {
+			int a;
+			cin >> a;
+			list.push(a);
+		}
+		else if (word == "pop") {
+			if (list.empty() == true) {
+				cout << "-1" << "\n";
+			}
+			else {
+				cout << list.front() << "\n";
+				list.pop();
+			}
+		}
+		else if (word == "size") {
+			cout << list.size() << "\n";
+		}
+		else if (word == "empty") {
+			if (list.empty() == true) {
+				cout << "1" << "\n";
+			}
+			else {
+				cout << "0" << "\n";
+			}
+		}
+		else if (word == "front") {
+			if (list.empty() == true) {
+				cout << "-1" << "\n";
+			}
+			else {
+				cout << list.front() << "\n";
+			}
+		}
+		else if (word == "back") {
+			if (list.empty() == true) {
+				cout << "-1" << "\n";
+			}
+			else {
+				cout << list.back() << "\n";
+			}
+		}
+	}
+	return 0;
+}
