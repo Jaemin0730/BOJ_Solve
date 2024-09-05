897dea951d04344ea704cb8fbfd8aed55fc096b6
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:23:21 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <memory.h>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <map>
+#include <list>
+#include <cmath>
+#include <set>
+using namespace std;
+#define ll long long
+
+void z()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main(void)
+{
+    z();
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::";
+    string s;
+    cin >> s;
+    cout << s;
+    cout << "::fan:\n";
+    cout << ":fan::fan::fan:";
+}
