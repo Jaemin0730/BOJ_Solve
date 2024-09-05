f95e4b708988dffbecb5a8fa546b1c7cab23386c
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:26:17 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL);
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+int main() {
+	cout << 2000 << "\n";
+    for (int i=1; i<=1000; i++) {
+        cout << "1" << " ";
+    }
+    for (int i=1; i<=1000; i++) {
+        cout << "1000" << " ";
+    }
+}
