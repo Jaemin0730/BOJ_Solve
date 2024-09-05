1abb9720201fd0083e2335c6e2d3f04ffae240ee
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 13:49:41 +0900

    [Bronze V] Title: Pyramids, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5341. Pyramids/Pyramids.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL);
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+
+
+int main(void)
+{
+    u
+    while (true) {
+        int n; 
+        cin >> n;
+        if (n == 0) break;
+        int k = n * (n + 1) / 2;
+        cout << k << "\n";
+    }
+}
