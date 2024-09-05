a112b50173079a5ee062e800aaa5dcb1f5826540
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2024-09-05 14:46:45 +0900

    [Bronze IV] Title: Большой удой, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26057. Большой удой/Большой удой.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL);
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+
+
+int main(void)
+{
+    u
+    int n, m;
+    cin >> n >> m;
+    cout << 2 * m - n;
+}
