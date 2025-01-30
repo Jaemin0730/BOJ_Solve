23e06622d522138ed6e2fedb3733d9e1ccf04397
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2025-01-31 03:20:16 +0900

    [Bronze I] Title: 차량 번호판 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/16968. 차량 번호판 1/차량 번호판 1.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL);
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+int main() {
+    int sol = 1;
+    string s;
+    cin >> s;
+    for (int i=0; i<s.size(); i++) {
+        if (s[i] == 'c') {
+            if (s[i - 1] == 'c') {
+                sol *= 25;
+            }
+            else {
+                sol *= 26;
+            }
+        }
+        else {
+            if (s[i - 1] == 'd') {
+                sol *= 9;
+            }
+            else {
+                sol *= 10;
+            }
+        }
+    }
+    cout << sol;
+}
