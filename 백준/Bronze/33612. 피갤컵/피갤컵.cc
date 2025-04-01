961dbf919dae5fe9746dc82d602a3802bcbd3a1e
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2025-04-02 00:34:46 +0900

    [Bronze V] Title: 피갤컵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33612. 피갤컵/피갤컵.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL);
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main() {
+    u
+    int n;
+    cin >> n;
+    if (n == 1) {
+        cout << 2024 << " " << 8;
+    }
+    else if (n == 2) {
+        cout << 2025 << " " << 3;
+    }
+    else if (n == 3) {
+        cout << 2025 << " " << 10;
+    }
+    else if (n == 4) {
+        cout << 2026 << " " << 5;
+    }
+    else if (n == 5) {
+        cout << 2026 << " " << 12;
+    }
+}
