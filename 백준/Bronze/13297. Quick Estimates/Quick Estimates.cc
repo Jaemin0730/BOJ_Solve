3a68354480bbed21edb144bb834f025783fdf3a7
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2025-05-05 23:57:30 +0900

    [Bronze IV] Title: Quick Estimates, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+
+ 
+int main() {
+    string a;
+    int n;
+    cin >> n;
+    while (n--) {
+        cin >> a;
+        cout << a.size() << '\n';
+    }
+}
