3b82cb4bb325435430eab3ff1b90cf1feac8ece6
김재민(Kim JaeMin) <97747502+Jaemin0730@users.noreply.github.com>
2026-01-29 04:14:42 +0900

    [Bronze V] Title: 이중 반복문, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34849. 이중 반복문/이중 반복문.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long
+
+int main(void)
+{
+    u;
+    int n;
+    cin >> n;
+    if (n <= 10000) {
+        cout << "Accepted";
+    }
+    else {
+        cout << "Time limit exceeded";
+    }
+}
